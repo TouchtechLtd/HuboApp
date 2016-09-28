49d5b521cde0b8d159f90539cc01fbb422e9b1c6
Richard Norton <richard@pixolut.com>
2016-09-28 17:09:48 +1300

    Add styles and fix testing bug.

--- Hubo/Hubo/App.xaml.cs
+++ Hubo/Hubo/App.xaml.cs
@@ -2,16 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Hubo
 {
-    public class App : Application
+    public partial class App : Application
     {
-        AppViewModel appVM = new AppViewModel();
-
         public App()
         {
+            InitializeComponent();
             MainPage = new NavigationPage(new HomePage());
         }
 
@@ -31,3 +31,4 @@ namespace Hubo
         }
     }
 }
+
