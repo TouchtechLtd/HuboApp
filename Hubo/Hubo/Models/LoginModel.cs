7f9836455fda930ca394a1a8186ab5af03a9898b
Ben Suarez-Brodie <benabelardosuarez@gmail.com>
2016-12-14 11:45:11 +1300

    Start implementation of login

---
+++ Hubo/Hubo/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hubo
+{
+    class LoginModel
+    {
+        public string usernameOrEmailAddress { get; set; }
+        public string password { get; set; }
+    }
+}
--- Hubo/Hubo/Services/RestService.cs
+++ Hubo/Hubo/Services/RestService.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using ModernHttpClient;
 using Newtonsoft.Json;
+using System.IO;
+using System.Net;
 
 namespace Hubo
 {
@@ -22,30 +18,63 @@ namespace Hubo
             this.db = new DatabaseService();
         }
 
-        internal bool Login(string username, string password)
+        internal Task<bool> Login(string username, string password)
         {
             //TODO: Code to communicate with server to login
             UserTable user = new UserTable();
-            user.User = username;
-            user.FirstName = "Ben";
-            user.LastName = "Suarez-Brodie";
-            user.Email = "ben@triotech.co.nz";
-            user.License = "DJ89473KL";
-            user.LicenseVersion = "158";
-            user.Endorsements = "F";
-            user.CompanyName = "Trio Technology";
-            user.Address = "41 The Square, Palmerston North";
-            user.CompanyEmail = "nick@triotech.co.nz";
-            user.Phone = "0278851100";
+            //user.User = username;
+            //user.FirstName = "Ben";
+            //user.LastName = "Suarez-Brodie";
+            //user.Email = "ben@triotech.co.nz";
+            //user.License = "DJ89473KL";
+            //user.LicenseVersion = "158";
+            //user.Endorsements = "F";
+            //user.CompanyName = "Trio Technology";
+            //user.Address = "41 The Square, Palmerston North";
+            //user.CompanyEmail = "nick@triotech.co.nz";
+            //user.Phone = "0278851100";
+            username = "Developer";
+            password = "D3v@triotech";
 
-            if(db.Login(user))
+            //using (var client = new HttpClient(new NativeMessageHandler()))
+            //{
+            ////    var content = new FormUrlEncodedContent(new[]
+            ////    {
+            ////    new KeyValuePair<string,string>("usernameOrEmailaddress", username),
+            ////    new KeyValuePair<string,string>("password", password)
+            ////});
+
+
+
+
+            //    var huboApiTokenUrl = GetBaseUrl() + Constants.REST_URL_LOGIN;
+            //    HttpResponseMessage authenticateResponse;
+            //    authenticateResponse = await client.PostAsJsonAsync(new Uri(huboApiTokenUrl), content);
+            //    if (authenticateResponse.IsSuccessStatusCode)
+            //    {
+            //        return true;
+            //    }
+            //    return false;
+            //}
+
+            LoginModel login = new LoginModel();
+            login.usernameOrEmailAddress = username;
+            login.password = password;
+            string json = JsonConvert.SerializeObject(login);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(GetBaseUrl() + Constants.REST_URL_LOGIN);
+            request.ContentType = "application/json";
+            request.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
             {
-                return true;
+                streamWriter.Write(json);
             }
-            else
-            {
-                return false;
-            }
+        }
+
+        private string GetBaseUrl()
+        {
+            return "http://192.168.1.112:6634/api";
         }
 
         internal void QueryUpdateUserInfo(UserTable user)
