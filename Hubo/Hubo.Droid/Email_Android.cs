b0b7c3a63132931ec6aca73be6fa1c6fba08e7a5
TRIOTECH\danieln <daniel@triotech.co.nz>
2016-12-15 09:33:49 +1300

    trying to fix the code to add attachment to email

---
+++ Hubo/Hubo.Droid/Email_Android.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+using Xamarin.Forms;
+using Hubo.Droid;
+
+[assembly: Dependency(typeof(Email_Android))]
+
+namespace Hubo.Droid
+{
+    
+    class Email_Android : IEmail
+    {
+        public Email_Android()
+        {
+
+        }
+
+        public bool Email(string mailTo, string subject, List<string> filePaths)
+        {
+            //try
+            //{
+                Context context = Android.App.Application.Context;
+
+                var email = new Intent(Intent.ActionSendMultiple);
+                email.SetType("message/rfc822");
+                email.PutExtra(Intent.ExtraEmail, mailTo);
+                email.PutExtra(Intent.ExtraSubject, subject);
+
+                var uris = new List<IParcelable>();
+                filePaths.ForEach(file =>
+                {
+                    var fileIn = new File(file);
+                    var uri = Android.Net.Uri.FromFile(fileIn);
+                    uris.Add(uri);
+                });
+
+                email.PutParcelableArrayListExtra(Intent.ExtraStream, uris);
+
+                context.StartActivity(email);
+
+                return true;
+            //}
+            //catch
+            //{
+            //    return false;
+            //}
+        }
+    }
+}
