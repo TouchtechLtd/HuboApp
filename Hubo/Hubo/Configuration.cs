25821b740dcffb7cdbc496773ba12e33d20aa423
TRIOTECH\danieln <daniel@triotech.co.nz>
2017-04-03 17:12:42 +1200

    change hockey for ios

--- Hubo/Hubo/Configuration.cs
+++ Hubo/Hubo/Configuration.cs
@@ -9,7 +9,7 @@ namespace Hubo
         public static readonly string DBname = "Hubo";
         public static readonly string APKpath = "Hubo\\Hubo.Droid\\bin\\Debug\\triotech.hubo.droid.apk";
         public static readonly string HockeyAppIdDroid = "12a779a468a644978640a05adf53d308";
-        public static readonly string HockeyAppIdIOS = "7c332e92aa634d2cb095cf7f2ea698a1";
+        public static readonly string HockeyAppIdIOS = "46b5aeca8ad64e66a1aa685251126a48";
         public static readonly string GoogleMapsApiKey = "AIzaSyC6s9hclMumJvehV4rr5SpXDxO5NXME45I";
     }
 }
